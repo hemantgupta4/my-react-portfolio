e51e3cdff17df2085591c109ee5fac55f0e603e1
Hemant Gupta <78254044+hemantgupta4@users.noreply.github.com>
2024-03-17 06:37:08 +0530

    Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/hemantgupta4"
+                href="https://github.com/hemantgupta4/my-react-portfolio/"
                 target="_blank"
                 className="fork-btn-inner"
               >
